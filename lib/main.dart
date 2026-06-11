e0437d866640f2b31115d78c8c95c5decb45e194
Wenderson Artur <150379511+WarturS@users.noreply.github.com>
2026-06-11 00:44:59 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'screens/login_screen.dart';
-
 void main() {
   runApp(const SmartCultoApp());
 }
-
 class SmartCultoApp extends StatelessWidget {
   const SmartCultoApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'SmartCulto',
-
-      // Localizações
-      localizationsDelegates: const [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-
-      supportedLocales: const [
-        Locale('pt', 'BR'),
-        Locale('en', 'US'),
-      ],
-
-      locale: const Locale('pt', 'BR'),
-
       theme: ThemeData(
-        useMaterial3: true,
+        fontFamily: 'Inter',
+        primaryColor: const Color(0xFF0D47A1),
         colorScheme: ColorScheme.fromSeed(
           seedColor: const Color(0xFF0D47A1),
+          primary: const Color(0xFF0D47A1),
         ),
-        scaffoldBackgroundColor: Colors.white,
+        useMaterial3: true,
       ),
-
       home: const LoginScreen(),
     );
   }
