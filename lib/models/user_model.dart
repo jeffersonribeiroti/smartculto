9b7ac277439e889293b4cd8bbedda910e175b9bb
Wenderson Artur <150379511+WarturS@users.noreply.github.com>
2026-06-11 01:15:41 -0300

    Update user_model.dart

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -3,11 +3,13 @@ class UserModel {
   final String church;
   final String email;
   final String password;
+  final String role; // 'admin' ou 'recepcionista'
 
   UserModel({
     required this.name,
     required this.church,
     required this.email,
     required this.password,
+    required this.role,
   });
 }
